4da3c88f72803d90aa5636e729ed22a4ac3e2c89
TimofeyBM <borodin2505@gmail.com>
2023-08-22 18:31:36 +0300

    new module all update

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -1,236 +1,83 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Core {
+import "./Clients.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Core is CreateClients{
     address owner;
-    mapping(address=>bool) public ownersBank;
-    address public BANK;
-    constructor(){
+    constructor(address clientInfo_) CreateClients(clientInfo_){
         owner = msg.sender;
     }
-    struct client{
-        string name;
-        address clientOwner;
-        address bank;
-        uint clientType;
-        bool accountDebet;
-        bool accountCredit;
-        bytes32[] paymentInfo;
-        document[] documents;
-        uint balanceUsdt;
-        mapping(address=>uint) tokenBalance;
-        uint balanceCredit;
-    }
-    struct document{
-        uint typeDocument;
-        string documentInfo;
-        address document;
-    }
-    mapping(address=>mapping(uint=>bool)) initSubscribe;
-    mapping(address=>client) private clients;
-    mapping(address=>address) oracles;
-
     modifier onlyOwner() {
         require(owner == msg.sender, "faild");
         _;
     }
-    modifier onlyOwnerBank(address sender){
-        require(ownersBank[sender] == true);
-        _;
-    }
-    function checkOwnerBank() public view returns(bool){
-        return ownersBank[msg.sender];
-    }
-    modifier checkTransfer(address sender, address to) {
-        require(clients[sender].clientOwner == sender, "not client");
-        require(clients[to].clientOwner == to, "not client");
-        require(msg.sender == BANK, "not bank");
-        require(clients[sender].accountDebet == true, "not open account");
-        require(clients[to].accountDebet == true, "not open account");
-        require(clients[to].bank == clients[sender].bank, "different bank");
-        _;
-    }
-    function transferBalance(uint amount, address token, address to, address sender) public checkTransfer(sender, to){
-        (, int256 price, , , ) = AggregatorV3Interface(oracles[token]).latestRoundData();
-        require(price > 0, "Invalid price");
-        unchecked {
-            // need decimals 
-            clients[sender].balanceUsdt -= (amount / 1000000) * (uint(price) / 100); 
-            clients[sender].tokenBalance[token] -= amount;
-            clients[to].balanceUsdt += (amount / 1000000) * (uint(price) / 100);
-            clients[to].tokenBalance[token] += amount;
-            
-        }
-    }
-    function addNeworacle(address token, address _oracle) public onlyOwner(){
-        oracles[token] = _oracle;
-    }
     
-    //updateBalanceInUSD()???...
-    //function transferFromBank()???...
-    
+
+    function transferBalance(uint amount, address token, address to, address sender) public{
+        (, address isOwnerAccount, address bank, , bool debet,) = clientInfo.clients(sender);
+        (, address isOwnerAccountTo, address bankTo, , bool debetTo,) = clientInfo.clients(to);
+        require(isOwnerAccount == sender, "not client");
+        require(isOwnerAccountTo == to, "not client");
+        require(msg.sender == clientInfo.BANK(), "not bank");
+        require(debet == true, "not open account");
+        require(debetTo == true, "not open account");
+        require(bank == bankTo, "different bank");
+        clientInfo.updateBalanceInUSD(sender);
+        clientInfo.transferBalanceInBank(sender, to, token, amount, "new transfer balance");
+    }
+    function investing(address sender, address token, uint amount) public{
+        clientInfo.invest(sender, token, amount);
+    }
     function deposit(uint amount, address token, address sender) public {
-        require(BANK == msg.sender, "not bank");
-        require(clients[sender].clientOwner == sender, "not client");
-        require(clients[sender].accountDebet == true, "not open account");
+        (, address isOwnerAccount, , , bool debet,) = clientInfo.clients(sender);
+        require(clientInfo.BANK() == msg.sender, "not bank");
+        require(isOwnerAccount == sender, "not client");
+        require(debet == true, "not open account");
         require(IERC20(token).allowance(sender, address(this)) >= amount, "chek allowance");
-        IERC20(token).transferFrom(sender, BANK, amount);
-        (, int256 price, , , ) = AggregatorV3Interface(oracles[token]).latestRoundData();
-        require(price > 0, "Invalid price");
-        clients[sender].balanceUsdt += (amount / 1000000) * (uint(price) / 100);
-        clients[sender].tokenBalance[token] += amount;
-
+        clientInfo.updateBalanceInUSD(sender);
+        IERC20(token).transferFrom(sender, letterOfCreditAddress, amount);
+        clientInfo.depositBalance(sender, token, amount);
     }
-    function withdrawal(uint amount, address token, address oracle, address sender) public{
-        require(BANK == msg.sender, "not bank");
-        require(clients[sender].clientOwner == sender, "not client");
-        require(clients[sender].accountDebet == true, "not open account");
+    function withdrawal(uint amount, address token, address sender) public{
+        (, address isOwnerAccount, , , bool debet,) = clientInfo.clients(sender);
+        require(clientInfo.BANK() == msg.sender, "not bank");
+        require(isOwnerAccount == sender, "not client");
+        require(debet == true, "not open account");
         require(IERC20(token).allowance(msg.sender, address(this)) >= amount, "chek allowance");
-        IERC20(token).transferFrom(msg.sender, sender, amount);
-        (, int256 price, , , ) = AggregatorV3Interface(oracle).latestRoundData();
-        require(price > 0, "Invalid price");
-        clients[sender].balanceUsdt -= (amount / 1000000) * (uint(price) / 100);
-        clients[sender].tokenBalance[token] -= amount;
+        clientInfo.updateBalanceInUSD(sender);
+        clientInfo.withdrawlBalance(sender, token, amount);
     }  
-    function balanceTokenAccount(address tokenAddress, address oracle, address sender) public view returns(uint, uint){
-        require(clients[sender].clientOwner == sender, "not client");
-        uint balance = IERC20(tokenAddress).balanceOf(sender);
-        (, int256 price, , , ) = AggregatorV3Interface(oracle).latestRoundData();
-        require(price > 0, "Invalid price");   
-        return (balance, balance*(uint(price)/100));
-    }
-    function createBank() public{
-        BANK = msg.sender;
-    }
-    function setOwner(address _owner) public{
-        require(msg.sender == BANK);
-        ownersBank[_owner] = true;
-    }
-
-    function initializeClient(string calldata _name, address sender) public{
-        require(BANK == msg.sender, "not bank");
-        require(clients[sender].bank != msg.sender || clients[sender].clientOwner != sender, "already exist");
-        client storage newClientInfo = clients[sender];
-        newClientInfo.name = _name;
-        newClientInfo.clientOwner = sender;
-        newClientInfo.bank = msg.sender;
-        newClientInfo.accountDebet = false;
-        newClientInfo.accountCredit = false;
-
-        uint size;
-        
-        assembly {
-            size := extcodesize(sender)
-        }
-        if (size > 0) {
-            bytes4 expectedFunctionSignature = bytes4(keccak256("check(uint256,address)"));
-            (bool success, ) = sender.call(abi.encodeWithSelector(expectedFunctionSignature, 1, sender));
-            require(success, "faild");
-            if(success == true){
-                newClientInfo.clientType = 1;
-                newClientInfo.documents.push(document(0, "entity person", sender));
-            } 
-        } else{
-            newClientInfo.clientType = 0;
-            newClientInfo.documents.push(document(0, "natural person", sender));
-        }
-        newClientInfo.paymentInfo.push(bytes32(keccak256(bytes("Create client"))));    
-    }
-    function getClientInfo(address _client) public view returns(string memory, address, address, bytes32[] memory, uint, uint, uint) {
-        return (clients[_client].name, clients[_client].clientOwner, clients[_client].bank, clients[_client].paymentInfo, clients[_client].balanceUsdt, clients[_client].tokenBalance[0xdAC17F958D2ee523a2206206994597C13D831ec7], clients[_client].tokenBalance[0x514910771AF9Ca656af840dff83E8264EcF986CA]);
-    }
-    function addAccountDebet(address sender) public{  
-        require(sender == clients[sender].clientOwner, "Not client4");
-        initSubscribe[sender][0] = true;
-    }
-    function addAccountCredit(address sender) public{  
-        require(sender == clients[sender].clientOwner);
-        initSubscribe[sender][1] = true;
-    }
-    function complitesubscribersAccount(address _client, uint typeAccount, address sender) public onlyOwnerBank(sender){
-        require(initSubscribe[_client][typeAccount] == true, "not complite");
-        require(typeAccount == 0 || typeAccount == 1, "error");
-        if(typeAccount == 0){
-            clients[_client].accountDebet = true;
-            initSubscribe[_client][typeAccount] = false;
-        }
-        if(typeAccount == 1){
-            clients[_client].accountCredit = true;
-            initSubscribe[_client][typeAccount] = false;
-        }
-    }
-    function deletAccountDebet(address sender) public{
-        require(sender == clients[sender].clientOwner);
-        require(clients[sender].accountDebet == true, "not open");
-        initSubscribe[sender][0] = true;
-    }
-    function deletAccountCredit(address sender) public{
-        require(sender == clients[sender].clientOwner);
-        require(clients[sender].accountCredit == true, "not open");
-        initSubscribe[sender][1] = true;
-    }
-    function comliteDeletAccount(address _client, uint typeAccount, address sender) public onlyOwnerBank(sender){
-        require(initSubscribe[_client][typeAccount] == true, "not complite");
-        require(typeAccount == 0 || typeAccount == 1, "error");
-        if(typeAccount == 0){
-            clients[_client].accountDebet = false;
-            initSubscribe[_client][typeAccount] = false;
-        }
-        if(typeAccount == 1){          
-            clients[_client].accountCredit = false;
-            initSubscribe[_client][typeAccount] = false;
-        }
-    }
-   
     
-}
+    // function makeLettersOfCredit_(address sender, address recipient, uint amount, address token, string calldata contitions) public {
+    //     require(clients[sender].clientOwner == sender, "not client");
+    //     require(clients[recipient].clientOwner == recipient, "not client");
+    //     updateBalanceInUSD(sender);
+    //     letterOfCredit.makeLettersOfCredit(sender, clients[sender].bank, clients[sender].tokenBalance[token], clients[sender].balanceUsdt[token], oracles[token],  recipient, amount, token, contitions);
+    // }
+    // function confirmLetterOfCredit_(address sender) public{
+    //     letterOfCredit.confirmLetterOfCredit(sender, msg.sender);
+    // }
+    // function sendLettersOfCredit_(address sender) public{
+    //     letterOfCredit.sendLettersOfCredit(sender);
+    // }
+      
+    // function getClientInfo21(address sender) public view returns(string memory,address, address, uint, uint, uint){
+    //     return letterOfCredit.getClientInfo12(sender);
+    // }
+    // function checkLetterOfCredit_(address sender) public{
+    //     letterOfCredit.checkLetterOfCredit(sender);
+    // }
+    // function sendMoney_(address sender) public{
+    //     letterOfCredit.sendMoney(sender);
+    // }
+    // function compliteLetterOfCredit_(address sender) public{
+    //     letterOfCredit.compliteLetterOfCredit(sender);
+    // }
 
-contract OtherContract {
-    address public coreAddress; 
-    Core public coreInstance; 
-    constructor(address _coreAddress) {
-        coreAddress = _coreAddress;
-        coreInstance = Core(coreAddress);
-    }
-    function checkClient(address client) public view returns(string memory,address, address, bytes32[] memory, uint, uint, uint){
-        return coreInstance.getClientInfo(client);
-    }
-    function setOwnerBank() public{
-        coreInstance.createBank();
-    }
-    function setOwnerClient(address owner_)  public{    
-        coreInstance.setOwner(owner_);
-    }
-    function openAccount(address client, uint typeAccount) public{
-        coreInstance.complitesubscribersAccount(client, typeAccount, msg.sender);
-    }
-    function addAccountDebet() public{
-       
-        coreInstance.addAccountDebet(msg.sender);
-    }
-    function newClient(string calldata _name) public{
-        coreInstance.initializeClient(_name, msg.sender);
-    }
-    function balance(address token) public view returns(uint){
-        return IERC20(token).balanceOf(msg.sender);
-    }
+    // function checkLetter_(address sender) public view returns(address, address, uint, address, string memory, uint){
+    //     return letterOfCredit.checkLetter(sender);
+    // }  
 
-    function depositeToBank(uint amount, address token) public{
-        //approve frontend
-        coreInstance.deposit(amount, token, msg.sender);
-    }
-    function getWETHPriceInUSD(address oracle) public view returns (uint256) {
-        (, int256 price, , , ) = AggregatorV3Interface(oracle).latestRoundData();
-        require(price > 0, "Invalid price");
-        return uint256(price) / 100; //делим на 100 так как decimals оракла 8 а у usdt decimals 6 => 8-6=2 лишние нули
-    }
-     function transferBalanceInBank(uint amount, address token, address to) public{
-         coreInstance.transferBalance(amount, token, to, msg.sender);
-     }
-
-     function checkAllowance(address token) public view returns(uint){
-        return IERC20(token).allowance(msg.sender, address(this));
-     }
 }
--- test/Core.js
+++ test/Core.js
@@ -9,50 +9,68 @@ describe("ABS vendor", function () {
     beforeEach(async function() {
       [owner, buyer, client] = await ethers.getSigners()
 
+      const clientInfo = await ethers.getContractFactory("ClientInfo", owner)
+      info = await clientInfo.deploy()
+      await info.deployed() 
+
+      const letterOfcredit = await ethers.getContractFactory("LettersOfCredit", owner)
+      letter = await letterOfcredit.deploy(info.address)
+      await letter.deployed() 
+     
       const ABS = await ethers.getContractFactory("Core", owner)
-      core = await ABS.deploy()
+      core = await ABS.deploy(info.address)
       await core.deployed()
+     
       const Test = await ethers.getContractFactory("OtherContract", owner)
-      contractTest = await Test.deploy(core.address)
-      await contractTest.deployed()    
+      contractTest = await Test.deploy(core.address, letter.address, info.address)
+      await contractTest.deployed() 
+
       const swapToken = await ethers.getContractFactory("EthToTokenSwap", owner)
       swap = await swapToken.deploy()
       await swap.deployed()
+      
+
+      console.log(letter.address);
       console.log(contractTest.address)
       console.log(core.address);
       console.log(owner.address);
+      info.addModules(core.address)
+      info.addModules(letter.address)
+      info.addModules(contractTest.address)
     })
 
     it("should create new client", async function() {
       await contractTest.setOwnerBank();
-      const tx = await contractTest.newClient("New client")
-      //console.log(await core.getClientInfo(owner.address));
+      await contractTest.newClient("New client")
+      console.log(await contractTest.checkClient(owner.address));
     })
     it("should create new account", async function() {
       await contractTest.setOwnerBank();
+      console.log(await contractTest.checkClient(contractTest.address));
+      await contractTest.setOwnerClient(owner.address)
       const tx = await contractTest.newClient("New client")
-      
-      // const tx1 = await contractTest.addAccountDebet()
-      // const tx2 = await contractTest.complitesubscribersAccount(contractTest.address, 0)
-      // const tx11 = await contractTest.addAccountCredit()
-      // const tx22 = await contractTest.complitesubscribersAccount(contractTest.address, 1)
-      //console.log(await core.getClientInfo(owner.address));
+      const tx1 = await contractTest.addAccountDebet_()
+      const tx2 = await contractTest.openAccount(owner.address, 0)
+      const tx11 = await contractTest.addAccountCredit_()
+      const tx22 = await contractTest.openAccount(owner.address, 1)
+      console.log(await contractTest.checkClient(owner.address));
     })
     it("should delet new account", async function() {
-      await contractTest.setOwnerBank();
-      const tx = await contractTest.connect(buyer).newClient("New client")
-      
-      // await contractTest.connect(buyer).addAccountDebet()
-      // await contractTest.complitesubscribersAccount(contractTest.address, 0)
-      // await contractTest.connect(buyer).addAccountCredit()
-      // await contractTest.complitesubscribersAccount(contractTest.address, 1)
-      // console.log(await contractTest.connect(buyer).getClientInfo(buyer.address));
-      // await contractTest.connect(buyer).deletAccountDebet()
-      // await contractTest.comliteDeletAccount(contractTest.address, 0)
-      // await contractTest.connect(buyer).deletAccountCredit()
-      // await contractTest.comliteDeletAccount(contractTest.address, 1)
+        await contractTest.setOwnerBank();
+        console.log(await contractTest.checkClient(contractTest.address));
+        await contractTest.setOwnerClient(owner.address)
+        const tx = await contractTest.newClient("New client")
+        const tx1 = await contractTest.addAccountDebet_()
+        const tx2 = await contractTest.openAccount(owner.address, 0)
+        const tx11 = await contractTest.addAccountCredit_()
+        const tx22 = await contractTest.openAccount(owner.address, 1)
+        console.log(await contractTest.checkClient(owner.address));
+        await contractTest.deletAccountDebet_()
+        await contractTest.deletAccount(owner.address, 0)
+        await contractTest.deletAccountCredit_()
+        await contractTest.deletAccount(owner.address, 1)
 
-      //console.log(await core.connect(buyer).getClientInfo(buyer.address));
+        console.log(await contractTest.checkClient(buyer.address));
     })
   
     it("should deposit token", async function () {
@@ -60,9 +78,9 @@ describe("ABS vendor", function () {
       const tx = await contractTest.connect(buyer).newClient("New client1")
       console.log(await contractTest.connect(buyer).checkClient(buyer.address));
       await contractTest.setOwnerClient(owner.address)
-      await contractTest.connect(buyer).addAccountDebet()
+      await contractTest.connect(buyer).addAccountDebet_()
       await contractTest.openAccount(buyer.address, 0)
-      await core.addNeworacle("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
+      await contractTest.addNeworacle_("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
       await swap.connect(buyer).swapETHForTokens("0xdAC17F958D2ee523a2206206994597C13D831ec7", {value: ethers.utils.parseEther("1")})
       console.log("amount token", await swap.connect(buyer).getTokenBalance("0xdAC17F958D2ee523a2206206994597C13D831ec7"))
       const usdtABI = [
@@ -139,9 +157,9 @@ describe("ABS vendor", function () {
       await contractTest.connect(buyer).newClient("New client1")
      
       await contractTest.setOwnerClient(owner.address)
-      await contractTest.connect(buyer).addAccountDebet()
+      await contractTest.connect(buyer).addAccountDebet_()
       await contractTest.openAccount(buyer.address, 0)
-      await core.addNeworacle("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
+      await contractTest.addNeworacle_("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
       await swap.connect(buyer).swapETHForTokens("0xdAC17F958D2ee523a2206206994597C13D831ec7", {value: ethers.utils.parseEther("1")})
       console.log("amount token", await swap.connect(buyer).getTokenBalance("0xdAC17F958D2ee523a2206206994597C13D831ec7"))
       const usdtAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7"; 
@@ -156,7 +174,7 @@ describe("ABS vendor", function () {
       console.log("allowance", await contractTest.connect(buyer).checkAllowance("0xdac17f958d2ee523a2206206994597c13d831ec7"));
       await contractTest.connect(buyer).depositeToBank(amount, "0xdac17f958d2ee523a2206206994597c13d831ec7")
       await contractTest.connect(client).newClient("New client2")
-      await contractTest.connect(client).addAccountDebet()
+      await contractTest.connect(client).addAccountDebet_()
       await contractTest.openAccount(client.address, 0)
       console.log(await contractTest.connect(client).checkClient(client.address));
       console.log(await contractTest.connect(buyer).checkClient(buyer.address));
@@ -164,5 +182,127 @@ describe("ABS vendor", function () {
       console.log(await contractTest.connect(buyer).checkClient(buyer.address), await contractTest.connect(client).checkClient(client.address));
       
     });
+    it("should show correct balance", async function() {
+      const usdtABI = [
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "_spender",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        
+        ] 
+      await contractTest.setOwnerBank();
+      await contractTest.newClient("New client")
+      await contractTest.setOwnerBank();
+      await contractTest.connect(buyer).newClient("New client1")
+     
+      await contractTest.setOwnerClient(owner.address)
+      await contractTest.connect(buyer).addAccountDebet_()
+      await contractTest.openAccount(buyer.address, 0)
+      await contractTest.addNeworacle_("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
+      await swap.connect(buyer).swapETHForTokens("0xdAC17F958D2ee523a2206206994597C13D831ec7", {value: ethers.utils.parseEther("1")})
+      console.log("amount token", await swap.connect(buyer).getTokenBalance("0xdAC17F958D2ee523a2206206994597C13D831ec7"))
+      const usdtAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7"; 
+      const usdtContract = new ethers.Contract(usdtAddress, usdtABI, buyer);
     
+      const amount = ethers.utils.parseUnits("0.645", 6);
+    
+      const spenderAddress = core.address 
+    
+      const tx3 = await usdtContract.connect(buyer).approve(spenderAddress, amount);
+      await tx3.wait();
+      console.log("allowance", await contractTest.connect(buyer).checkAllowance("0xdac17f958d2ee523a2206206994597c13d831ec7"));
+      await contractTest.connect(buyer).depositeToBank(amount, "0xdac17f958d2ee523a2206206994597c13d831ec7")
+      console.log("AMOUNT MONEY", await contractTest.connect(buyer).checkClient(buyer.address));
+      })
+      it("should check letter of credit client", async function() {
+           const usdtABI = [
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "_spender",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        
+        ] 
+        await contractTest.setOwnerBank();
+        await contractTest.connect(client).newClient("New client")
+        await contractTest.connect(buyer).newClient("New client1")
+
+        console.log(await contractTest.connect(client).checkClient(client.address));
+        console.log(await contractTest.connect(buyer).checkClient(buyer.address));
+
+        await contractTest.setOwnerClient(owner.address)
+
+        await contractTest.connect(buyer).addAccountDebet_()
+        await contractTest.openAccount(buyer.address, 0)
+        await contractTest.connect(client).addAccountDebet_()
+        await contractTest.openAccount(client.address, 0)
+
+        await contractTest.addNeworacle_("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D")
+        
+        await swap.connect(buyer).swapETHForTokens("0xdAC17F958D2ee523a2206206994597C13D831ec7", {value: ethers.utils.parseEther("1")})
+        const amount = ethers.utils.parseUnits("100", 6);
+        const usdtContract = new ethers.Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7", usdtABI, buyer);
+        const tx3 = await usdtContract.connect(buyer).approve(core.address, amount);
+        await tx3.wait();
+        
+        await contractTest.connect(buyer).depositeToBank(amount, "0xdac17f958d2ee523a2206206994597c13d831ec7")
+        console.log("AMOUNT MONEY", await contractTest.connect(buyer).checkClient(buyer.address));
+
+        await contractTest.connect(buyer).investing_("0xdAC17F958D2ee523a2206206994597C13D831ec7", ethers.utils.parseUnits("50", 6))
+        console.log("AMOUNT MONEY", await contractTest.checkClient(contractTest.address));
+
+        await contractTest.connect(buyer)._makeLetterOfCerdit(client.address, 100000, "0xdac17f958d2ee523a2206206994597c13d831ec7", "transfer new phone to China fom USA. 100 phone on day 3 month")
+
+        await contractTest.connect(owner)._confirmLetterOfCredit(buyer.address);
+        console.log(await contractTest.connect(buyer)._checkLetter(buyer.address));
+
+        await contractTest.connect(owner)._sendLettersOfCredit(buyer.address);
+
+        await contractTest.connect(owner)._checkLetterOfCredit(buyer.address);
+
+        await contractTest.connect(owner)._sendMoney(buyer.address);
+        console.log(await contractTest.connect(buyer)._checkLetter(buyer.address));
+
+        await contractTest.connect(owner)._compliteLetterOfCredit(buyer.address);
+
+        console.log("AMOUNT MONEY", await contractTest.checkClient(contractTest.address));
+        console.log("AMOUNT MONEY", await contractTest.checkClient(client.address));
+       })
 })
